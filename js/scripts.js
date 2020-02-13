c9448f81c62e33074d32af637f633cf338e7716e
Noel Jengwe <noelsolomon131@yahoo.com>
2020-02-13 16:26:42 +0000

    Update a starter-code indext by adding menu and headind, update the css to make the in the nav to line center and also update the js to add event element to make the menu to output main.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = () => {
+  menuNav.classList.toggle('open');
+
+}
+
+ menuToggle.addEventListener('click', toggleMenu);
